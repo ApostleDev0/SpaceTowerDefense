e67025b55fa703b45387224c2b647945be24f1d3
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-11-13 21:09:05 +0700

    Path logic
    - create script for path
    - using GUIStyle for label and line, text, color

--- TowerDefense/Assets/Scripts/Path.cs
+++ TowerDefense/Assets/Scripts/Path.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Path : MonoBehaviour
@@ -12,7 +13,12 @@ public class Path : MonoBehaviour
         {
             for(int i = 0; i < Waypoints.Length; i++)
             {
-                if(i <  Waypoints.Length - 1)
+                GUIStyle style = new GUIStyle();
+                style.normal.textColor = Color.white;
+                style.alignment = TextAnchor.MiddleCenter;
+                Handles.Label(Waypoints[i].transform.position + Vector3.up * 0.7f, Waypoints[i].name, style);
+                
+                if (i <  Waypoints.Length - 1)
                 {
                     Gizmos.color = Color.gray;
                     Gizmos.DrawLine(Waypoints[i].transform.position, Waypoints[i+1].transform.position);
