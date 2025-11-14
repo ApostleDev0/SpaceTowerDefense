434a0ddc07887fb8c8947d8873cd46676e252630
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-11-14 11:09:43 +0700

    Update Enemy with Scriptable Objects
    - Remove [Serializefield] of current path for private
    - Create Scriptable Object
    - Create Enemy Data for each type of enemy
    - Remove [Serializefield] movespeed
    - Add [Serializefield] enemydata

---
+++ TowerDefense/Assets/ScriptableObjects/EnemyData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public float lives;
+    public float speed;
+    public int damage;
+}
--- TowerDefense/Assets/Scripts/Enemy/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy/Enemy.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private Path currentPath;
+    [SerializeField] private EnemyData data;
+    private Path _currentPath;
 
     private Vector3 _targetPosition;
     private int _currentWaypoints;
 
     private void Awake()
     {
-        currentPath = GameObject.Find("Path1").GetComponent<Path>();
+        _currentPath = GameObject.Find("Path1").GetComponent<Path>();
     }
 
     private void OnEnable()
     {
         _currentWaypoints = 0;
-        _targetPosition = currentPath.GetPosition(_currentWaypoints);
+        _targetPosition = _currentPath.GetPosition(_currentWaypoints);
     }
 
     // Update is called once per frame
     void Update()
     {
         // move toward to target position
-        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, data.speed * Time.deltaTime);
 
         // set new target position
         float relativeDistance = (transform.position - _targetPosition).magnitude;
         if(relativeDistance < 0.1f)
         {
-            if(_currentWaypoints < currentPath.Waypoints.Length - 1)
+            if(_currentWaypoints < _currentPath.Waypoints.Length - 1)
             {
                 _currentWaypoints++;
-                _targetPosition = currentPath.GetPosition(_currentWaypoints);
+                _targetPosition = _currentPath.GetPosition(_currentWaypoints);
             }
             else
             {
