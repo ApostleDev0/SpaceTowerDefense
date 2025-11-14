37b364511bc3261b7544efd4ff8b3d1c24703e59
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-11-14 12:42:02 +0700

    Structure Wave Data
    - Create Scriptable Object Wave Data
    - Create Folder Utils to using enum
    - From wave data create each waves of enemy type to spawn
    - Fix logic spawn to work with wave data
    - Add [Serializefield} WaveData[] waves
    - Add Waves: 1,2,3,...
    - Update Spawner Script:
    + Add Dictionary to match EnemyType with ObjectPooler
    + Add method Awake() and define _poolDictionary
    + Add Index for wave to start
    + Delete _timeInterval
    + Add CurrentWave => waves[_currentWaveIndex]
    - Fix method SpawnEnemy():
    + Using TryGetValue to check key pair for type enemies -> take from the pool contain
    + Using _spawnCounter to count enemies spawn each waves, next waves to go

--- TowerDefense/Assets/Scripts/Spawner.cs
+++ TowerDefense/Assets/Scripts/Spawner.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private ObjectPooler pool;
+    [SerializeField] private WaveData[] waves;
+    private int _currentWaveIndex = 0;
+    private WaveData CurrentWave => waves[_currentWaveIndex];
 
     private float _spawnTimer;
-    private float _timeInterval = 2f;
+    private float _spawnCounter;
+    private int _enemiesRemoved;
+
+    [SerializeField] private ObjectPooler goblinPool;
+    [SerializeField] private ObjectPooler flyingPool;
+    [SerializeField] private ObjectPooler heftyPool;
+
+    private Dictionary<EnemyType,ObjectPooler> _poolDictionary;
+
+    private void Awake()
+    {
+        _poolDictionary = new Dictionary<EnemyType, ObjectPooler>()
+        {
+            {EnemyType.Golbin, goblinPool },
+            {EnemyType.Flying, flyingPool },
+            {EnemyType.Hefty, heftyPool },
+        };
+    }
 
     // Update is called once per frame
     void Update()
     {
         // time down to 0 to spawn enemy
         _spawnTimer -= Time.deltaTime;
-        if( _spawnTimer <= 0 )
+        if( _spawnTimer <= 0 && _spawnCounter <= CurrentWave.enemiesPerWave)
         {
-            _spawnTimer = _timeInterval;
+            _spawnTimer = CurrentWave.spawnInterval;
             SpawnEnemy();
+            _spawnCounter++;
+        }
+        else if(_spawnCounter >= CurrentWave.enemiesPerWave && _enemiesRemoved >= waves.Length)
+        {
+            _currentWaveIndex = (_currentWaveIndex + 1) % waves.Length;
+            _spawnCounter = 0;
         }
     }
 
     private void SpawnEnemy()
     {
-        GameObject spawnedObject = pool.GetPooledObjected();
-        spawnedObject.transform.position = transform.position;
-        spawnedObject.SetActive(true);
+        if(_poolDictionary.TryGetValue(CurrentWave.enemyType, out var pool))
+        {
+            GameObject spawnedObject = pool.GetPooledObjected();
+            spawnedObject.transform.position = transform.position;
+            spawnedObject.SetActive(true);
+        }
+        
     }
 }
