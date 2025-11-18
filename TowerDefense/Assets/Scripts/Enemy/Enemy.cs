7c4a96c8bedcf52e3edb1c3a8e971bfb94ea9b05
Dev_Num0 <106236672+ApostleDev0@users.noreply.github.com>
2025-11-18 11:37:55 +0700

    Reward resources Gold
    - EnemyData Script
    + feat: Add resource Reward to show how many resource drop and get when enemy destroyed
    - Enemy Scripts
    + feat: Add Data public to read only variable from data
    - GameManagers Script
    + feat: Add _resources
    + feat: Add private method name AddResources contain amount of resource drop from enemy
    + feat: Add HandleEnemyDestroyed method to give resource from destroyed enemy
    + refractor: Call AddResources and use Mathf.RoundInt() to convert value float
    + refractor: Add subcrise for method OnEnable and OnDisable
    + feat: Add static event OnResourcesChanged and call it after calculate amount
    + feat: Add Invoke resources on Start method
    - UI Changes:
    + feat: Add ResourceText
    + feat: Add [Serialize] resource text
    + feat: Add UpdateResourcesText method to update UI
    + refractor: subcrise OnEnable and OnDisable for update resource text

--- TowerDefense/Assets/Scripts/Enemy/Enemy.cs
+++ TowerDefense/Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private EnemyData data;
+    public EnemyData Data => data;
+
     public static event Action<EnemyData> OnEnemyReachedEnd;
     public static event Action<Enemy> OnEnemyDestroyed;
 
--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -6,23 +6,37 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static event Action<int> OnLivesChanged;
+    public static event Action<int> OnResourcesChanged;
 
     private int _lives = 5;
+    private int _resource = 0;
     private void OnEnable()
     {
         Enemy.OnEnemyReachedEnd += HandleEnemyReachedEnd;
+        Enemy.OnEnemyDestroyed += HandleEnemyDestroyed;
     }
     private void OnDisable()
     {
         Enemy.OnEnemyReachedEnd -= HandleEnemyReachedEnd;
+        Enemy.OnEnemyDestroyed -= HandleEnemyDestroyed;
     }
     private void Start()
     {
         OnLivesChanged?.Invoke(_lives);
+        OnResourcesChanged?.Invoke(_resource);
     }
     private void HandleEnemyReachedEnd(EnemyData data)
     {
         _lives = Mathf.Max(0, _lives - data.damage);
         OnLivesChanged?.Invoke(_lives);
+    }
+    private void HandleEnemyDestroyed(Enemy enemy)
+    {
+        AddResources(Mathf.RoundToInt(enemy.Data.resourceReward));
+    }
+    private void AddResources(int amount)
+    {
+        _resource += amount;
+        OnResourcesChanged?.Invoke(_resource);
     }
 }
